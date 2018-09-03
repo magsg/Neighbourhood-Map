ef82d71d207a9be7fed2200a52c364aa098c6a3e
Malgorzata <mw.gradowska@gmail.com>
2018-09-03 11:48:30 +0100

    add responsiveness to map, create pin component for markers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MapGL, {Marker, NavigationControl} from 'react-map-gl';
 import './App.css';
+import Pin from './pin';
 
 
 class App extends Component {
@@ -25,9 +26,30 @@ this.state = {
 
 componentDidMount() {
         this.getPlaces();
+        window.addEventListener('resize', this.resize);
+        this.resize();
     }
 
-getPlaces() {
+    componentWillUnmount() {
+    window.removeEventListener('resize', this.resize);
+  }
+
+  resize = () => {
+  this.setState({
+    container: {
+      ...this.state.container,
+      width: this.props.width || window.innerWidth,
+      height: this.props.height || window.innerHeight
+    }
+  });
+};
+
+updateViewport = (container) => {
+   this.setState({container});
+ }
+
+
+getPlaces = () => {
      let url = 'https://api.foursquare.com/v2/venues/explore?'
      let parameters = {
          client_id: 'VSHIOMXPUT1YOHRSEI2I5FJ1FWHT2JNFAY3ZOGV5MZJ10WGJ',
@@ -35,7 +57,7 @@ getPlaces() {
          v: "20180323",
          section: 'food',
          ll: '50.0, 19.9',
-         limit: '7'
+         limit: '2'
      }
 
      fetch(url + new URLSearchParams(parameters))
@@ -48,22 +70,32 @@ getPlaces() {
          console.log(this.state.places); //it's fetching!
      })
      .catch(error => alert('error!' + error))
- }
 
+  }
+
+  marker = (place) => {
+    return (
+      <Marker key = {place.venue.id}
+      longitude = {place.venue.location.lat}
+      latitude = {place.venue.location.lng}>
+      <Pin size = {40}/>
+      </Marker>
+    );
+  }
 
   render() {
-
-const {container} = this.state;
 
     return (
       <main>
       <MapGL
-      {...container}
+      {...this.state.container}
       mapStyle= 'mapbox://styles/mapbox/streets-v10'
-      mapboxApiAccessToken='pk.eyJ1Ijoia290ZWs2IiwiYSI6ImNqam42MmFnejF0aXYza20wdXh4dGFwcXcifQ.e-GDBXL7FGLyrbtdyy-gkw'>
+      mapboxApiAccessToken='pk.eyJ1Ijoia290ZWs2IiwiYSI6ImNqam42MmFnejF0aXYza20wdXh4dGFwcXcifQ.e-GDBXL7FGLyrbtdyy-gkw'
+      onViewportChange={(container) => this.setState({container})}>
       <div className="nav">
         <NavigationControl/>
       </div>
+      {this.state.places.map(this.marker)}
     </MapGL>
       </main>
 
