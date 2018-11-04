29a1c988afc4a57d28f90819d988e79ac3c6d402
Malgorzata <mw.gradowska@gmail.com>
2018-11-04 12:59:58 +0000

    add filter to search bar

--- src/Venue-list.js
+++ src/Venue-list.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class VenueListItem extends Component {
 
     constructor(props) {
     super(props);
     this.state = {
-      query: ""
+      query: ''
     }
   }
+
+//trims off any extra white space from user input  
 
 updateQuery = (query) =>{
   this.setState({query: query.trim()})
@@ -17,6 +21,17 @@ render() {
 
   const {venueItem, stateChange} = this.props;
 
+//adds filter functionality to search bar
+
+  let showingVenues
+  if(this.state.query) {
+    const match = new RegExp(escapeRegExp(this.state.query), 'i')
+    showingVenues = venueItem.filter((place) => match.test(place.venue.name))
+  } else {
+    showingVenues = venueItem
+  }
+
+//returns a search bar and a list of venues from foursquare api
 
   return (
     <div className = "listing-box">
@@ -28,7 +43,7 @@ render() {
     onChange = {(event) => this.updateQuery(event.target.value)}/>
     </div>
     <ol className = "list-places">
-    {venueItem.map((place) => (
+    {showingVenues.map((place) => (
       <li key = {place.venue.id}
       onClick = {() => {stateChange(place)}}
       > {place.venue.name} </li>
